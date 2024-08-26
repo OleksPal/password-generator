b75ca77dbf962bec5caac46079ae77dff9f155de
User <oleksandr.palamarchuk1310@gmail.com>
2024-08-26 20:45:18 +0300

    Change the generation algorithm for the non-memorable password to more distributable

--- PasswordGeneratorApp/PasswordGenerator/PasswordGenerator.cs
+++ PasswordGeneratorApp/PasswordGenerator/PasswordGenerator.cs
@@ -20,8 +20,26 @@ namespace PasswordGeneratorApp.Generator
         {
             var password = String.Empty;
 
+            //var symbolType = new string[3] { "Letters", "Numbers", "SpecialSymbols" };
+            //int optionNumber = 0;
+            //if(Vocabulary.n)
+
             for (int i = 0; i < passwordSize; i++)
-                password += Vocabulary[random.Next(Vocabulary.Length)];
+            {
+                int symbolTypeChoice = random.Next(3);
+                switch (symbolTypeChoice)
+                {
+                    case 0:
+                        password += Constants.Letters[random.Next(Constants.Letters.Count())];
+                        break;
+                    case 1:
+                        password += Constants.Numbers[random.Next(Constants.Numbers.Count())];
+                        break;
+                    case 2:
+                        password += Constants.SpecialSymbols[random.Next(Constants.SpecialSymbols.Count())];
+                        break;
+                }
+            }
 
             return password;
         }
